75860ae5834cc91a702e504f1577a058b1b90954
German Poljakov <gerpol17@gmail.com>
2018-12-19 15:30:23 +0100

    logging added; config updated; raw value wrapped to avoid missing key for co2

--- tinymesh_api/tinymesh_client.py
+++ tinymesh_api/tinymesh_client.py
@@ -1,6 +1,8 @@
 import requests
+
 from requests.auth import HTTPBasicAuth
 from requests.status_codes import codes as status_codes
+
 import json
 import logging
 import config
@@ -16,9 +18,10 @@ class TinyMeshClient:
         self.VERSION = config.TINYM['API_VERSION']
 
         self.CHUNK_SIZE = 8096
+        self.NETWORK_ID = config.TINYM['NETWORK_ID']
 
     def subscribe(self):
-        url = '%s/%s/%s/%s' % (self.BASE_URL, self.VERSION, 'messages', 'SHR')
+        url = '%s/%s/%s/%s' % (self.BASE_URL, self.VERSION, 'messages', self.NETWORK_ID)
 
         params = {
             'query': '',
@@ -31,8 +34,24 @@ class TinyMeshClient:
 
         if response.status_code == status_codes.OK:
             for chunk in response.iter_content(self.CHUNK_SIZE):
+
+                logging.info('Message received from network id \'%s\' (length: %d bytes)', self.NETWORK_ID, len(chunk))
+
+                # convert message to json
                 message = json.loads(chunk)
+
                 if 'meta' not in message:
+                    # if the message is not meta, it contains sensor readings
                     yield message
+                else:
+                    # otherwise it is the first message from the server
+                    logging.info('Successfully subscribed to Tinymesh network \'%s\'', self.NETWORK_ID)
         else:
+            # status code is not 200
+            logging.error('Tinymesh Cloud responded with status %d', response.status_code)
+            body = json.loads(response.text)
+            if 'error' in body:
+                # attach the error message from the server (if any)
+                logging.error('Error message: %s', body['error'])
+
             return False
--- view_azure/view_azure.py
+++ view_azure/view_azure.py
@@ -1,6 +1,7 @@
 import base64
 
 from azure.eventhub import EventHubClient, Sender, EventData
+from azure.eventhub.common import EventHubError
 
 import logging
 import config
@@ -9,11 +10,12 @@ import json
 
 class ViewAzureClient:
     def __init__(self):
-        self.sas_key = config.ENV['AZURE_SAS_KEY']
-        self.sas_user = config.ENV['AZURE_SAS_USER']
+        self.SAS_KEY = config.ENV['AZURE_SAS_KEY']
+        self.SAS_USER = config.ENV['AZURE_SAS_USER']
 
-        self.logger = logging.getLogger('azure')
-        self.logger.setLevel('DEBUG')
+        self.ENTITY_PATH = config.AZURE['ENTITY_PATH']
+        self.ENDPOINT = config.AZURE['ENDPOINT']
+        self.PARTITION = config.AZURE['PARTITION']
 
         self.client = None
         self.sender = None
@@ -21,15 +23,24 @@ class ViewAzureClient:
     def run(self):
         self.client = EventHubClient.from_connection_string(
             'Endpoint=%s;SharedAccessKeyName=%s;SharedAccessKey=%s;EntityPath=%s'
-            % (config.AZURE['ENDPOINT'], self.sas_user, self.sas_key, config.AZURE['ENTITY_PATH'])
+            % (self.ENDPOINT, self.SAS_USER, self.SAS_KEY, self.ENTITY_PATH)
         )
-        self.sender = self.client.add_sender(partition='0')
+        self.sender = self.client.add_sender(partition=self.PARTITION)
 
         # client.run() returns a 'failed' boolean
-        return not self.client.run()
+
+        status = False
+
+        try:
+            status = not self.client.run()
+        except EventHubError as e:
+            logging.error(str(e))
+        finally:
+            return status
 
     def stop(self):
         self.client.stop()
+        logging.warning('Azure Client has been stopped')
 
     def publish(self, message):
         key = message['key'].split('-', 4)[-1]
@@ -44,16 +55,27 @@ class ViewAzureClient:
             # prepare the lambda function which converts the raw value to unit value
             convert_to_unit = formatting_rule['conversion_fn']
 
+            property_name = formatting_rule['property_name']
+            unit = formatting_rule['unit']
+
             # build the device body
             device = {
                 'deviceid': device_id,
-                'property': formatting_rule['property_name'],
-                'unit': formatting_rule['unit'],
+                'property': property_name,
+                'unit': unit,
                 'values': [],
             }
 
             # get the raw value recorded in the sensor
-            raw_value = body[formatting_rule['key_name']]
+            selector = formatting_rule['key_name']
+            if selector not in body:
+                # skip a property if the sensor did not contain the necessary key
+                logging.warning('Device \'%s\' missing key \'%s\' for reading \'%s\'. \'%s\' will therefore be skipped',
+                                device_id, selector, property_name, property_name)
+
+                continue
+
+            raw_value = body[selector]
 
             # convert the value to unit value
             value = {
@@ -67,7 +89,13 @@ class ViewAzureClient:
             # add the device dict to the array of devices
             devices.append(device)
 
+        # convert to string with correct json format
+        event_message = json.dumps({'devices': devices})
+
+        logging.info('Device \"%s\" publishing event to EventHub \"%s\" (message length: %d bytes)',
+                     device_id, self.ENTITY_PATH, len(event_message))
+
         # send devices object to Azure server
-        event = {'devices': devices}
-        response = self.sender.send(EventData(str(event)))
-        print(device_id + ':\n\n' + response.name)
+        response = self.sender.send(EventData(event_message))
+
+        logging.info('EventHub \"%s\" responded with status %s.', self.ENTITY_PATH, response.name)
